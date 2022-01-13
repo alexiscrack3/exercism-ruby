a08c6fb55f7b060b0357cabfd7a6476dd0d22b47
Alexis Ortega <alexis.oc3@gmail.com>
2022-01-13 14:17:54 -0700

    Update simple calculator exercise

--- simple-calculator/simple_calculator.rb
+++ simple-calculator/simple_calculator.rb
@@ -22,3 +22,19 @@ class SimpleCalculator
     "#{first_operand} #{operation} #{second_operand} = #{result}"
   end
 end
+
+# class SimpleCalculator
+#   class UnsupportedOperation < StandardError
+#   end
+#   ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
+#   def self.calculate(first_operand, second_operand, operation)
+#     raise ArgumentError if first_operand.instance_of?(String) || second_operand.instance_of?(String)
+#     raise UnsupportedOperation unless ALLOWED_OPERATIONS.include? operation
+
+#     begin
+#       "#{first_operand} #{operation} #{second_operand} = #{first_operand.send(operation, second_operand)}"
+#     rescue ZeroDivisionError => e
+#       'Division by zero is not allowed.'
+#     end
+#   end
+# end
